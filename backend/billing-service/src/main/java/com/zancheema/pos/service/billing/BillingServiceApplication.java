17be2f627ee7c3b2161a2f9c930e887ff45d3613
Zain Cheema <dev.zaincheema@gmail.com>
2024-05-15 00:46:05 +0100

    added discovery service and integrated with billing and inventory

--- backend/billing-service/src/main/java/com/zancheema/pos/service/billing/BillingServiceApplication.java
+++ backend/billing-service/src/main/java/com/zancheema/pos/service/billing/BillingServiceApplication.java
@@ -2,8 +2,10 @@ package com.zancheema.pos.service.billing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BillingServiceApplication {
 
 	public static void main(String[] args) {
